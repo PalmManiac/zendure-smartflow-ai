76b5e0a759dbc0fb3e35bc4946637a4e15add972
PalmManiac <84145136+PalmManiac@users.noreply.github.com>
2025-12-23 11:25:42 +0100

    Update select.py

--- custom_components/zendure_smartflow_ai/select.py
+++ custom_components/zendure_smartflow_ai/select.py
@@ -1,56 +1,36 @@
-from __future__ import annotations
-
-from typing import Any
-
 from homeassistant.components.select import SelectEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.restore_state import RestoreEntity
 
-from .const import DOMAIN, AI_MODES, MODE_AUTOMATIC
-from .coordinator import ZendureSmartFlowCoordinator
-
+from .const import *
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
-    coordinator: ZendureSmartFlowCoordinator = hass.data[DOMAIN][entry.entry_id]
-    async_add_entities([ZendureAIModeSelect(coordinator, entry)])
+    async_add_entities([ZendureAIModeSelect(entry)])
 
 
-class ZendureAIModeSelect(SelectEntity, RestoreEntity):
-    _attr_has_entity_name = True
-    _attr_name = "Moduswahl"
-    _attr_icon = "mdi:toggle-switch-variant"
-    _attr_options = AI_MODES
+class ZendureAIModeSelect(SelectEntity):
+    _attr_name = "Zendure SmartFlow AI Modus"
+    _attr_icon = "mdi:robot"
+    _attr_options = [
+        "Automatik",
+        "Sommer",
+        "Winter",
+        "Manuell",
+    ]
 
-    def __init__(self, coordinator: ZendureSmartFlowCoordinator, entry: ConfigEntry) -> None:
-        self._coordinator = coordinator
-        self._entry = entry
+    def __init__(self, entry: ConfigEntry):
         self._attr_unique_id = f"{entry.entry_id}_ai_mode"
-        self._current = MODE_AUTOMATIC
+        self._attr_current_option = "Automatik"
 
     @property
-    def device_info(self) -> dict[str, Any]:
-        return self._coordinator.device_info
+    def current_mode(self) -> str:
+        return {
+            "Automatik": AI_MODE_AUTO,
+            "Sommer": AI_MODE_SUMMER,
+            "Winter": AI_MODE_WINTER,
+            "Manuell": AI_MODE_MANUAL,
+        }[self._attr_current_option]
 
-    async def async_added_to_hass(self) -> None:
-        await super().async_added_to_hass()
-
-        last = await self.async_get_last_state()
-        if last and last.state in AI_MODES:
-            self._current = last.state
-        else:
-            self._current = MODE_AUTOMATIC
-
-        self._coordinator.settings.ai_mode = self._current
-        self.async_write_ha_state()
-
-    @property
-    def current_option(self) -> str:
-        return self._current
-
-    async def async_select_option(self, option: str) -> None:
-        if option not in AI_MODES:
-            return
-        self._current = option
-        self._coordinator.settings.ai_mode = option
+    async def async_select_option(self, option: str):
+        self._attr_current_option = option
         self.async_write_ha_state()
