fa817bdc92e062fa8ee7ec2d2059a6248a9534d6
PalmManiac <84145136+PalmManiac@users.noreply.github.com>
2026-01-02 11:22:09 +0000

    Update sensor.py

--- custom_components/zendure_smartflow_ai/sensor.py
+++ custom_components/zendure_smartflow_ai/sensor.py
@@ -61,6 +61,12 @@ SENSORS: tuple[ZendureSensorEntityDescription, ...] = (
         runtime_key="debug",
         icon="mdi:bug",
     ),
+    ZendureSensorEntityDescription(
+        key="decision_reason",
+        translation_key="decision_reason",
+        runtime_key="decision_reason",
+        icon="mdi:head-question-outline",
+    ),
     # Numeric sensors
     ZendureSensorEntityDescription(
         key="house_load",
@@ -146,10 +152,3 @@ class ZendureSmartFlowSensor(SensorEntity):
 
     async def async_added_to_hass(self) -> None:
         self.async_on_remove(self.coordinator.async_add_listener(self.async_write_ha_state))
-
-    ZendureSensorEntityDescription(
-        key="decision_reason",
-        translation_key="decision_reason",
-        runtime_key="decision_reason",
-        icon="mdi:head-question-outline",
-    ),
